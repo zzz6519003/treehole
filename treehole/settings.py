b17d5901ec8bfc37469367eef3eb08075d1f43a5
zzz6519003 <zhengzhongzhao@gmail.com>
2013-04-29 19:33:10 +0800

    sh

--- treehole/settings.py
+++ treehole/settings.py
@@ -37,7 +37,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+
         'NAME': os.path.join(DJANGO_ROOT_DIR, 'main.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
